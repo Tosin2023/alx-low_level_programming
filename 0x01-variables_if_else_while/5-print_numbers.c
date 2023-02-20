07261c3f3d6098842084fa48852f09995d9d3159
Tosin2023 <olamideanifowose42@gmail.com>
2023-02-19 23:52:07 -0800

    base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print all single digit number for base ten
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
