842894a53ba57a8f267058736aa4aa676328c191
Tosin2023 <olamideanifowose42@gmail.com>
2023-02-20 16:10:53 -0800

    twotwo digit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - A program that print two two digit
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; j <= 99; j++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j && i != j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
