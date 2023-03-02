bc0399a49529580cac059fb3648b16b296e4bb30
Tosin2023 <olamideanifowose42@gmail.com>
2023-03-02 09:33:29 -0800

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,6 +14,7 @@ int main(void)
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statment
+	 *
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
